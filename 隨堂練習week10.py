f8ca0cc92070005f54f91c46585e1ef884382c35
holofan <71250527+holofan@users.noreply.github.com>
2020-11-16 14:49:49 +0800

    Add files via upload

---
+++ 隨堂練習week10.py
@@ -0,0 +1,49 @@
+#1.變數賦值
+x=1
+
+##用等號=讓電腦給記憶空間
+##等號左邊
+##等號右邊
+
+##有什麼用處?
+
+#2.數字資料+數學運算
+##運算子  七種方式//%**
+##函數
+X=2
+Y=5
+print(X+Y)
+print(X-Y)
+print(X*Y)
+print(X/Y)
+print(X//Y)
+print(X%Y)
+print(X**Y)
+Z=X*Y
+print(Z)
+
+magic_number=22**3+17//5+17%5
+print(magic_number)
+
+#4.電腦輸出
+##print()函數
+print("神奇數字為",magic_number)
+
+#3.電腦輸入
+##input("Hint)函數
+
+num=input("Enter a number?")
+num=int(num)
+num=num+1
+print(num)
+
+num=(int(input("Enter a number?"))+1)
+num=num+1
+print(num)
+
+
+num=int(input("Enter a number"))+1
+print(num)
+
+
+print(int(input("Enter a number?"))+1)
