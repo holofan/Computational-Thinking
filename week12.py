2ae266bc68e4b0a57da54b915cbd28e9f6cdb5f6
holofan <71250527+holofan@users.noreply.github.com>
2020-11-30 14:50:36 +0800

    Add files via upload

--- week12.py
+++ week12.py
@@ -1,27 +1,15 @@
-hello1="Hello world"
-hello2="Hello python"
-hello3="Shaqile O'Neil"
+season=1995-1996
+team="Chicago Bulls"
+coach="Phil Jackson"
+records=[72,10]
+starting_lineup=["Ron Harper","Michael Jordan","Scottie Pippen","Dennis Rodman",
+"Luc Longley"]
+won_championship=True
 
-s1="123"
-s2="456"
-s3=s1+s2
-print(s3)
-s4=s1*2
-print(s4)
+best_chicago_bulls=list((season,team,coach,records,
+starting_lineup,won_championship))
+print(best_chicago_bulls)
+print(type(best_chicago_bulls))
 
-s1='An apple a day.'
-s1.capitalize()
-s1.title()
-s1.swapcase()
-s1.upper()
-s1.lower()
-
-numberlist=[1,2,3,4,5]
-charlist=["1","2","3","4","5"]
-mixlist=[1,"2",3,"4",5]
-
-class(numberlist)
-
-word="python"
-list1=list(word)
-charlist=["milk tea","games","novels","smartphone","steak"]
+#indexing=索引
+print(best_chicago_bulls[O])
