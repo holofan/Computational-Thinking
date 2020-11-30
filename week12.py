8a539b0b2e5323782d64e03bab8052313685bac1
holofan <71250527+holofan@users.noreply.github.com>
2020-11-30 14:40:12 +0800

    Add files via upload

---
+++ week12.py
@@ -0,0 +1,27 @@
+hello1="Hello world"
+hello2="Hello python"
+hello3="Shaqile O'Neil"
+
+s1="123"
+s2="456"
+s3=s1+s2
+print(s3)
+s4=s1*2
+print(s4)
+
+s1='An apple a day.'
+s1.capitalize()
+s1.title()
+s1.swapcase()
+s1.upper()
+s1.lower()
+
+numberlist=[1,2,3,4,5]
+charlist=["1","2","3","4","5"]
+mixlist=[1,"2",3,"4",5]
+
+class(numberlist)
+
+word="python"
+list1=list(word)
+charlist=["milk tea","games","novels","smartphone","steak"]
