5d4d58070731968636eef56c7ed3d44113d24561
holofan <71250527+holofan@users.noreply.github.com>
2020-12-14 14:30:10 +0800

    Add files via upload

---
+++ 隨堂練習week14.py
@@ -0,0 +1,43 @@
+start=int(input(''))
+end=int(input(''))
+step=int(input(''))
+sum=0
+for i in range(start,end,step):
+    sum=sum+i
+    print
+
+sheet=['牛奶','蛋','咖啡豆']
+for index in [0,1,2]:
+    print(index)
+0
+1
+2
+sheet=['牛奶','蛋','咖啡豆']
+for index in sheet
+    print(index)
+牛奶
+蛋
+咖啡豆
+
+
+sheet=['牛奶','蛋','咖啡豆']
+for index in[0,1,2]:
+    print(index,sheet[index])
+牛奶
+蛋
+咖啡豆
+sheet=['牛奶','蛋','咖啡豆']
+for index in range(0,len(sheet)):
+
+numbers = [123, 456, 789]
+ for i in numbers:
+ print(i) 
+123
+456
+789
+ numbers = [123, 456, 789]
+ for i in range(0, len(numbers)):
+ print(i, numbers[i])
+0 123
+1 456
+2 789
