9e11b41db0c242be0078c033b02eda9e209de80e
holofan <71250527+holofan@users.noreply.github.com>
2020-12-21 14:53:45 +0800

    Add files via upload

--- 隨堂練習week14.py
+++ 隨堂練習week14.py
@@ -1,43 +1,49 @@
-start=int(input(''))
-end=int(input(''))
-step=int(input(''))
-sum=0
-for i in range(start,end,step):
-    sum=sum+i
-    print
-
-sheet=['牛奶','蛋','咖啡豆']
-for index in [0,1,2]:
-    print(index)
-0
-1
-2
-sheet=['牛奶','蛋','咖啡豆']
-for index in sheet
-    print(index)
-牛奶
-蛋
-咖啡豆
+#import turtle
+#shelly = turtle.Turtle()
+#shelly.circle(120)
 
 
-sheet=['牛奶','蛋','咖啡豆']
-for index in[0,1,2]:
-    print(index,sheet[index])
-牛奶
-蛋
-咖啡豆
-sheet=['牛奶','蛋','咖啡豆']
-for index in range(0,len(sheet)):
+#import turtle
+#shelly = turtle.Turtle()
 
-numbers = [123, 456, 789]
- for i in numbers:
- print(i) 
-123
-456
-789
- numbers = [123, 456, 789]
- for i in range(0, len(numbers)):
- print(i, numbers[i])
-0 123
-1 456
-2 789
+#shelly.hideturtle()
+
+#for i in range (4)
+  #  shelly.forward(250)
+  #  shelly.left(90)
+  #  print(1)
+    
+import turtle
+shelly = turtle.Turtle()
+
+shelly.color("red")
+
+for i in range(6)
+    shelly.forward(250)
+    shelly.left(90)
+    print(i)
+    
+import turtle
+shelly = turtle.Turtle()
+colors = ["red","green","blue","black","purple","yellow"]
+
+for n in range(6):
+    shelly.color(colors[n])
+    shelly.begin_fill()
+    shelly.fillcolor(colors[n])
+    
+for i in range(4)
+    shelly.forward(250)
+    shelly.left(90)
+    shelly.right(60)
+    shelly.end_fill()
+    
+import turtle
+shelly = turtle.Turtle()
+
+shelly.color("yellow")
+shelly.pensize(5)
+
+for i in range(5)
+    shelly.forward(250)
+    shelly.right(144)
